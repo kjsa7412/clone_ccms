b42438c850c701af3e7910b81d125953cc32f229
kjsa <kjsa7412@gmail.com>
2022-08-01 16:11:42 +0900

    Init

---
+++ src/main/java/com/kjsa/clone_ccms/CloneCcmsApplication.java
@@ -0,0 +1,13 @@
+package com.kjsa.clone_ccms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloneCcmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloneCcmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kjsa/clone_ccms/CloneCcmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kjsa.clone_ccms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloneCcmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
