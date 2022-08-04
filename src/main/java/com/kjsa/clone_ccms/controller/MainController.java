79a551ea447d92a9605eb6c8e3c55b69317d1fff
kjsa <kjsa7412@gmail.com>
2022-08-04 18:00:44 +0900

    Test Commit - 220804_1
    Test Commit - 220804_1

--- src/main/java/com/kjsa/clone_ccms/controller/MainController.java
+++ src/main/java/com/kjsa/clone_ccms/controller/MainController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class MainController {
     @GetMapping(value = "/")
     public String index() {
-        return "index";
+        return "/index";
     }
 }
